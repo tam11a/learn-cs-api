03fa66a9023f8e7ed0afa4778f585faecbdc4a58
tam11a <ibrahimsadiktamim@gmail.com>
2022-11-28 21:08:49 +0600

    initialized: minimal web api

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PasteBin API", Description = "Share or store code from cloud", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
